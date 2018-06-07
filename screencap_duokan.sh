080f364d1cec3da359f8d7e1c4ac8580eb7cdf7a
刘寒 <1121797386@qq.com>
2018-06-07 14:35:35 +0800

    fix bug

--- screencap_duokan.sh
+++ screencap_duokan.sh
@@ -3,8 +3,8 @@
 #通过adb 左右翻页，同时截图到本地保存 
 #录入书名
 
-book_name='python自动化运维技术与最佳实践'
-count=315
+book_name='分析的力量'
+count=248
 #点击向右翻页
 swap_right(){
 	adb shell input tap 1050 800
@@ -21,34 +21,36 @@ screenshot(){
 	if [ ! -d "./$2" ]; then
 	  mkdir ./$2
 	fi
-	adb shell /system/bin/screencap -p /sdcard/$1.png
-	adb pull /sdcard/$1.png ./$2/
-	adb shell rm /sdcard/$1.png
+	# adb shell /system/bin/screencap -p /sdcard/$1.png
+	# adb pull /sdcard/$1.png ./$2/
+	# adb shell rm /sdcard/$1.png
+	adb exec-out screencap -p > ./$2/$1.png
 }
 
-
-for ((i=1; i<=$count; i ++))  
-do  
-	screenshot $i $book_name
-	swap_right
-	sleep 2
-done  
-
 #png 导出 pdf
+export_pdf(){
+	cd  ./$book_name
+	for ((i=1; i<=$count; i ++))  
+	do  
+		a=65
+		out=$[((i/10))+$a]
+		t=`printf "%x" $out`  
+		as="\\x$t" 
+		outname=$as-$i
+		echo $outname
+		mv ./$i.png ./$outname.png
+	done  
+	convert *.png $book_name.pdf	
+}
 
-cd  ./$book_name
-for ((i=1; i<=$count; i ++))  
-do  
-	a=97
-	out=$[((i/10))+$a]
-	t=`printf "%x" $out`  
-	as="\\x$t" 
-	outname=$as-$i
-	echo $outname
-	mv ./$i.png ./$outname.png
-done  
+main(){
+	for ((i=1; i<=$count; i ++))  
+	do  
+		screenshot $i $book_name
+		swap_right
+		sleep 1
+	done  
 
-convert *.png test.pdf
-
-#图片转文字
-#tesseract ./1.png 1 -l chi_sim
+	export_pdf
+}
+main
